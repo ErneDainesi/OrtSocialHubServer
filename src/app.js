031e5d7a9c5bc729248d31973aebe605b627cb3a
ErneDainesi <edainesi@fi.uba.ar>
2024-06-01 15:47:44 -0300

    separate app from index
    Now in the app.js file we will only set up everything we need for the
    server to work, and then use the index.js as the programs entry file

--- src/app.js
+++ src/app.js
@@ -1,20 +1,13 @@
 import express from 'express';
-import { sequelize } from './db/sequelize.js';
 import cors from 'cors';
-import { PORT } from './config.js';
+import routes from './routes/routes.js';
 
-const app = express()
+const app = express();
 
-app.use(cors())
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-await sequelize.sync({force:false})
+app.use('/', routes);
 
-app.get('/', (req, res) => {
-  res.send("Hello World!")
-})
-
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`)
-})
+export default app;
