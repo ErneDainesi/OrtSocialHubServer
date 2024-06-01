9e2aeebc097a99614cab5a3fb8e2c9a518c2c7e1
ErneDainesi <edainesi@fi.uba.ar>
2024-06-01 18:21:01 -0300

    add user model

---
+++ src/model/User.js
@@ -0,0 +1,32 @@
+import { DataTypes } from 'sequelize';
+import { sequelize } from '../db/sequelize.js';
+
+const User = sequelize.define(
+    'User',
+    {
+        firstName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        lastName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isEmail: true
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: true
+            }
+        }
+    }
+);
+
+export default User;
