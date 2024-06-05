858d5876df9e1939a87c7c2f554b22988a3fdea2
ErneDainesi <edainesi@fi.uba.ar>
2024-06-04 23:11:34 -0300

    add fetch home feed end point

--- src/controllers/post.js
+++ src/controllers/post.js
@@ -1,7 +1,8 @@
-import { Post } from "../model/models.js";
+import { Follower, Post, User } from "../model/models.js";
 
 export const post = async (req, res) => {
     try {
+        console.log(`Saving ${req.body}`);
         await Post.create(req.body);
         res.status(200).json({
             success: true
@@ -11,6 +12,33 @@ export const post = async (req, res) => {
         res.status(500).json({
             success: false,
             error: "Error while trying to save post"
+        });
+    }
+}
+
+export const fetchHomeFeed = async (req, res) => {
+    try {
+        const { userId } = req.params;
+        const following = await Follower.findAll({
+            where: {
+                followerId: userId
+            }
+        });
+        const posts = await Post.findAll({
+            where: {
+                UserId: following
+            },
+            order: ['createdAt', 'DESC']
+        });
+        res.status(200).json({
+            success: true,
+            posts
+        });
+    } catch(error) {
+        console.log(error);
+        res.status(500).json({
+            success: false,
+            error: "Error while trying to fetch posts"
         });
     }
 }
--- src/routes/post.js
+++ src/routes/post.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { post } from "../controllers/post.js";
+import { fetchHomeFeed } from "../controllers/post.js";
 
 const postRouter = Router();
 
 postRouter.post('/', post);
+postRouter.get('/home/:userId', fetchHomeFeed);
 
 export default registerRouter;
