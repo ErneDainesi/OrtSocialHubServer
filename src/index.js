5bdca70603db174abcb067c8385780ea22079568
ErneDainesi <edainesi@fi.uba.ar>
2024-06-04 21:45:17 -0300

    add try catch

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import App from './app.js';
 import { sequelize } from './db/sequelize.js';
 import { PORT } from './config.js';
 
-await sequelize.sync({force:false})
+try {
+    await sequelize.sync({force: false})
+} catch(error) {
+    console.log("Couldn't sync db");
+}
 
 App.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`)
