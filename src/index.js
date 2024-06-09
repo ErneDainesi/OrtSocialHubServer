d40bb0764064dc431b2d01bdaf9985653419128a
ErneDainesi <edainesi@fi.uba.ar>
2024-06-09 17:22:46 -0300

    add comment

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { sequelize } from './db/sequelize.js';
 import { PORT } from './config.js';
 
 try {
+    // Set {force: true} if each time you start server you want a clean db
     await sequelize.sync({force: false})
 } catch(error) {
     console.log("Couldn't sync db");
