40c50d8bdd66b66429ae050d6649c2511558be3d
ErneDainesi <edainesi@fi.uba.ar>
2024-06-12 20:29:23 -0300

    send jwt via token, couldnt get cookies to work

--- src/middleware/validateToken.js
+++ src/middleware/validateToken.js
@@ -2,12 +2,12 @@ import { verifyToken } from "../utils/token.js";
 
 export const validateToken = async (req, res, next) => {
     try {
-        const { token } = req.cookies;
+        const { token } = req.body;
         if (!token) throw new Error("Not logged in");
         const { payload } = verifyToken(token);
-        req.user = payload;
+        req.user = payload.user;
         next();
     } catch (error) {
-        res.status(400).send({ succces: false, message: error.message });
+        res.status(400).send({ succces: false, message: "Couldn't validate jwt" });
     }
 };
