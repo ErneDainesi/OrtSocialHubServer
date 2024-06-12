84739dcf18f4a5cef0ca96262a6a087a8c421e6e
ErneDainesi <edainesi@fi.uba.ar>
2024-06-12 20:31:47 -0300

    update register and login

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -4,6 +4,14 @@ import { generateToken } from "../utils/token.js";
 
 export const register = async (req, res) => {
     try {
+        const oldUser = await User.findOne({ where: { email: req.body.email } });
+        if (oldUser) {
+            res.status(200).json({
+                success: false,
+                error: "User with same email exists"
+            });
+            return;
+        }
         const hashedPass = await hash(req.body.password, 10);
         const user = await User.create({
             ...req.body,
@@ -49,10 +57,13 @@ export const login = async (req, res) => {
                 lastName: user.lastName
             }
         };
-        const token = generateToken(payload);
-        res.cookie("token", token);
+        const jwt = generateToken(payload);
+        res.cookie("token", jwt);
+        res.cookie("loggedInUserId", user.id);
         res.status(200).send({
-            success: true
+            success: true,
+            jwt,
+            loggedInUserId: user.id
         });
     } catch (error) {
         console.log(error);
