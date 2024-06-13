04d3199dbaace3b87e02b0c4807f8a77a7f00ef0
ErneDainesi <edainesi@fi.uba.ar>
2024-06-13 03:21:46 -0300

    use cookies and avoid sending jwt in request response

--- src/middleware/validateToken.js
+++ src/middleware/validateToken.js
@@ -2,7 +2,7 @@ import { verifyToken } from "../utils/token.js";
 
 export const validateToken = async (req, res, next) => {
     try {
-        const { token } = req.body;
+        const { token } = req.cookies;
         if (!token) throw new Error("Not logged in");
         const { payload } = verifyToken(token);
         req.user = payload.user;
