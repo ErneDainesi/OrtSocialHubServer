41f04d3d45c9616bd6b07c39ae33b7ad094d2aaf
ErneDainesi <edainesi@fi.uba.ar>
2024-06-18 16:02:39 -0300

    remove same site and secure

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -57,9 +57,7 @@ export const login = async (req, res) => {
         const jwt = generateToken(payload);
         res.cookie("jwt_token", jwt, {
             httpOnly: true,
-            maxAge: 1000 * 60 * 30, // the cookie will live for an hour
-            sameSite: 'None',
-            secure: true
+            maxAge: 1000 * 60 * 30 // the cookie will live for an hour
         });
         res.status(200).send({
             success: true,
