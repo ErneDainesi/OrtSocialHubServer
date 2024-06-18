2203e8b8271e64c44323c5a552801db3a288fa7e
ErneDainesi <edainesi@fi.uba.ar>
2024-06-18 11:19:11 -0300

    add fetch profile

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -10,7 +10,6 @@ export const register = async (req, res) => {
                 success: false,
                 error: "User with same email exists"
             });
-            return;
         }
         const hashedPass = await hash(req.body.password, 10);
         const user = await User.create({
@@ -39,7 +38,6 @@ export const login = async (req, res) => {
                 success: false,
                 error: "User does not exist"
             });
-            return;
         }
         const validatePassword = await compare(password, user.password);
         if (!validatePassword) {
@@ -47,7 +45,6 @@ export const login = async (req, res) => {
                 success: false,
                 error: "Invalid credentials"
             });
-            return;
         }
         const payload = {
             id: user.id,
@@ -74,3 +71,28 @@ export const login = async (req, res) => {
         });
     }
 }
+
+export const fetchUsersProfile = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const user = await User.findByPk(id, {
+            attributes: ['id', 'firstName', 'lastName']
+        });
+        if (!user) {
+            res.status(404).json({
+                success: false,
+                error: "User not found"
+            });
+        }
+        res.status(200).json({
+            success: true,
+            user
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            success: false,
+            error: "Error while trying to fetch users profile"
+        });
+    }
+};
