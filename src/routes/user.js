e9826d547f64fe10ad7dd718f0a3ebd456dd4d78
ErneDainesi <edainesi@fi.uba.ar>
2024-06-18 11:19:23 -0300

    add profile endpoint at user route

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { register, login } from "../controllers/user.js";
+import { register, login, fetchUsersProfile } from "../controllers/user.js";
 import { validateToken } from "../middleware/validateToken.js";
 
 const userRouter = Router();
@@ -7,6 +7,6 @@ const userRouter = Router();
 userRouter.post('/register', register);
 userRouter.post('/login', login);
 userRouter.use(validateToken);
-// registerRouter.get('/profile/:id', fetchUsersProfile);
+userRouter.get('/profile/:id', fetchUsersProfile);
 
 export default userRouter;
