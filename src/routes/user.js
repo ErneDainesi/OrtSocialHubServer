0be2d827d3fb7c3272e7019a5c2116c052bdfa40
owendonn1 <owendonn@gmail.com>
2024-06-21 23:41:19 -0300

    logout function on controller.users and add route

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { register, login, fetchUsersProfile } from "../controllers/user.js";
+import { register, login, fetchUsersProfile, logout } from "../controllers/user.js";
 import { validateToken } from "../middleware/validateToken.js";
 
 const userRouter = Router();
 
 userRouter.post('/register', register);
 userRouter.post('/login', login);
+userRouter.post('/logout', logout);
 userRouter.use(validateToken);
 userRouter.get('/profile/:id', fetchUsersProfile);
 
