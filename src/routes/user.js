dd84179289edee6f5951f1e78d8883c81db6d119
ErneDainesi <edainesi@fi.uba.ar>
2024-06-26 16:13:24 -0300

    make unfollow a delete request instead of post

--- src/routes/user.js
+++ src/routes/user.js
@@ -17,7 +17,7 @@ userRouter.post('/login', login);
 userRouter.use(validateToken);
 userRouter.get('/profile/:id', fetchUsersProfile);
 userRouter.post('/follow', followUser );
-userRouter.post('/unfollow', unfollowUser);
+userRouter.delete('/unfollow', unfollowUser);
 userRouter.get('/followers/:userId', getFollowing);
 userRouter.put('/profile', editUserProfile);
 
